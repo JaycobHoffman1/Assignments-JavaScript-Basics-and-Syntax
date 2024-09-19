e4638b510953c3136c97c7797c1812a128e8873e
JaycobHoffman1 <jaycobhoffman2004@gmail.com>
2024-09-18 19:03:52 -0700

    final commit

---
+++ Understanding Data Types in JavaScript/script.js
@@ -0,0 +1,17 @@
+// Task 1: Declare variables to represent student information such as name, age, grade, and attendance status
+let studentName;
+let studentAge;
+let studentGrade;
+let isPresent;
+
+// Task 2: Assign sample values to the student information variables
+studentName = "Alice";
+studentAge = 15;
+studentGrade = 10;
+isPresent = true;
+
+// Task 3: Display the student information using console.log() statements
+console.log(studentName)
+console.log(studentAge)
+console.log(studentGrade)
+console.log(isPresent)
